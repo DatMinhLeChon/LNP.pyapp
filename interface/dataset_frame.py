9a9f61306ebe3d414e47472d437863569ca623ac
lechonminhdat <dat.lemindast@gmail.com>
2023-04-23 19:41:57 +0700

    change

--- interface/dataset_frame.py
+++ interface/dataset_frame.py
@@ -19,13 +19,5 @@ class DatasetFrame(Frame):
         frame3 = Frame(self)
         frame3.pack(fill = BOTH)
         
-        sheet1 =tksheet.Sheet(frame3, data = [[]], height = 800, width = 1500)
-        sheet1.pack(fill=BOTH, pady=10, padx=5, expand=True)
-        sheet1.grid(row =20, column = 20,sticky="nswe")
-        sheet1.enable_bindings()
-        
-        Button1 = Button(frame1, text ="Run",width =15, command= partial(datasetApply, sheet1))
-        Button1.pack(side = BOTTOM, padx =5, pady=5)
-        
         Button2 = Button(frame2, text="End", width =5)
         Button2.pack(side = RIGHT, padx=5, pady =5)
