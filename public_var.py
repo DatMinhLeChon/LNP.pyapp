69e9846e9e329cf5d01ac6a303aa45f08b5be9dd
Dat <dat.lemindast@gmail.com>
2023-04-30 13:21:02 +0700

    change

--- public_var.py
+++ public_var.py
@@ -4,25 +4,25 @@ public_number_const = None
 public_number_val = None
 
 objective_type = None # 1 is maximize, 0 minimize
-public_obj = None
+public_obj = None #objective array 1D
 
 # inequal
-public_lhs_ineq = None
-public_rhs_ineq = None
+public_lhs_ineq = None # inequal left side array 2D
+public_rhs_ineq = None # inequal right side array 1D
 
 #equal
-public_lhs_eq = None
-public_rhs_eq = None
+public_lhs_eq = None # equal left side array 2D
+public_rhs_eq = None # equal right side array 1D
 
-public_method = ''
+public_method = '' #base: "hight-ds" method
 
-values = {"Maximize" : 1, "Minimize" : 0}
+values = {"Maximize" : 1, "Minimize" : 0} # value in radiobutton, configuration frame
 
 # Frame 
 root_main = None # global variable Frame
 root_temp = None # global variable Frame 
 
 # signal loop use to check dataset frame running
-signal_loop = 1
+signal_loop = 1 # check table create in main frame, if already init, close configuration frame 
 
-result_lnp = None
+result_lnp = None # model variable
